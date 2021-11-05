4994eefde6d93a40f2b87f6bcc31a9288b68b85e
Raghuboi <82239949+Raghuboi@users.noreply.github.com>
2021-11-05 07:25:12 +0530

    prettier setup

---
+++ components/layouts/main.js
@@ -0,0 +1,18 @@
+import Head from 'next/head'
+import { Box } from '@chakra-ui/react'
+const main = ({ children, router }) => {
+  return (
+    <Box as="main" pb={8}>
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <title>Raghunath Prabhakar- Home</title>
+      </Head>
+
+      <Container maxW="container.md" pt={14}>
+        {children}
+      </Container>
+    </Box>
+  )
+}
+
+export default Main
