34761d6a760ed535d8681ac0e2ea7bfede87688d
Raghuboi <82239949+Raghuboi@users.noreply.github.com>
2021-11-14 07:18:23 +0530

    update poker, add portfolio-website to project

--- pages/portfolio.js
+++ pages/portfolio.js
@@ -1,26 +1,49 @@
-import { Container, Heading, VStack, Alert, AlertIcon } from '@chakra-ui/react'
+import {
+  Container,
+  Heading,
+  VStack,
+  Alert,
+  AlertIcon,
+  useColorModeValue
+} from '@chakra-ui/react'
 import { ProjectItem } from '../components/project-item'
 import Layout from '../components/layouts/projects'
 import Section from '../components/section'
 
 import pokerInGame from '../public/images/portfolio/poker-in-game.png'
 import snakeInGame from '../public/images/portfolio/snake-in-game.png'
+import portfolioWebsiteThumbnail from '../public/images/portfolio/portfolio-website-thumbnail.png'
 
 const Portfolio = () => {
   return (
-    <Layout>
-      <Container pt={6}>
-        <Heading as="h3" fontSize={20} mb={4}>
-          Projects
-        </Heading>
-        <Section mb={3}>
-          <Alert borderRadius={20} status="info">
-            <AlertIcon />
-            Click on a Project for more information
-          </Alert>
-        </Section>
+    <Container pt={6}>
+      <Heading as="h3" variant="section-title" mb={6}>
+        Projects
+      </Heading>
+      <Alert
+        mb={3}
+        py={2}
+        borderRadius={20}
+        colorScheme="purple"
+        variant={useColorModeValue('solid', 'subtle')}
+        status="info"
+      >
+        <AlertIcon />
+        Click on a Project for more information
+      </Alert>
+      <Layout>
         <VStack>
-          <Section mb={1.5} delay={0.1}>
+          <Section mb={1.5} delay={0.025}>
+            <ProjectItem
+              id="portfolio-website"
+              title="Portfolio Website"
+              thumbnail={portfolioWebsiteThumbnail}
+            >
+              This very website. Fully responsive and server-side rendered using
+              Next.js
+            </ProjectItem>
+          </Section>
+          <Section mb={1.5} delay={0.05}>
             <ProjectItem
               id="poker"
               title="Multiplayer Poker Game"
@@ -29,15 +52,15 @@ const Portfolio = () => {
               WebSocket based Poker with User Authentication
             </ProjectItem>
           </Section>
-          <Section mb={1.5} delay={0.2}>
+          <Section mb={1.5} delay={0.075}>
             <ProjectItem id="snake" title="Snake AI" thumbnail={snakeInGame}>
               Re-creation of the classic Snake game given an AI with pathfinding
               algorithms
             </ProjectItem>
           </Section>
         </VStack>
-      </Container>
-    </Layout>
+      </Layout>
+    </Container>
   )
 }
 
