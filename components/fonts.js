6795a74266ad00484babc224909e7e22e2a53b11
Raghuboi <82239949+Raghuboi@users.noreply.github.com>
2022-11-27 15:03:50 +0530

    Update body font

--- components/fonts.js
+++ components/fonts.js
@@ -3,6 +3,7 @@ const Fonts = () => (
     @import url('https://fonts.googleapis.com/css2?family=M+PLUS+Rounded+1c:wght@300;700&display=swap');
     @import url('https://fonts.googleapis.com/css2?family=Sarabun&display=swap');
     @import url('https://fonts.googleapis.com/css2?family=Source+Sans+Pro&display=swap');
+    @import url('https://fonts.googleapis.com/css2?family=Figtree&display=swap');
   `}</style>
 )
 export default Fonts
