6c016ceac6a7acc85bf1741519fa940e12774f3f
Raghuboi <82239949+Raghuboi@users.noreply.github.com>
2022-12-01 14:58:35 +0530

    Fix scroll animation

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,3 @@
-import React from 'react'
 import {
   Container,
   Box,
@@ -109,41 +108,41 @@ const Page = () => {
           more to add. I am always thinking, constantly questioning, and forever
           learning."
         />
-        <MediaButtons ref={ButtonDiv} />
+        <Wrap ref={ButtonDiv} justify="center" spacing={4} my={3.5}>
+          <MediaButtons />
+        </Wrap>
       </Section>
     </Container>
   )
 }
 
-const MediaButtons = React.forwardRef((props, ref) => {
+const MediaButtons = () => {
   return (
     <>
-      <Wrap ref={ref} justify="center" spacing={4} my={3.5}>
-        <NextLink href="/portfolio">
-          <Button rightIcon={<ChevronRightIcon />} colorScheme="purple">
-            My portfolio
-          </Button>
-        </NextLink>
-        <a href="https://github.com/Raghuboi" target="_blank" rel="noreferrer">
-          <Button
-            leftIcon={<IoLogoGithub />}
-            bg={useColorModeValue('#2E2E2E', 'whiteAlpha.200')}
-            _hover={{
-              bg: useColorModeValue('#000000', '#454545')
-            }}
-            color="white"
-          >
-            GitHub
-          </Button>
-        </a>
-        <a href="https://github.com/Raghuboi" target="_blank" rel="noreferrer">
-          <Button leftIcon={<IoLogoLinkedin />} colorScheme="facebook">
-            LinkedIn
-          </Button>
-        </a>
-      </Wrap>
+      <NextLink href="/portfolio">
+        <Button rightIcon={<ChevronRightIcon />} colorScheme="purple">
+          My portfolio
+        </Button>
+      </NextLink>
+      <a href="https://github.com/Raghuboi" target="_blank" rel="noreferrer">
+        <Button
+          leftIcon={<IoLogoGithub />}
+          bg={useColorModeValue('#2E2E2E', 'whiteAlpha.200')}
+          _hover={{
+            bg: useColorModeValue('#000000', '#454545')
+          }}
+          color="white"
+        >
+          GitHub
+        </Button>
+      </a>
+      <a href="https://github.com/Raghuboi" target="_blank" rel="noreferrer">
+        <Button leftIcon={<IoLogoLinkedin />} colorScheme="facebook">
+          LinkedIn
+        </Button>
+      </a>
     </>
   )
-})
+}
 
 export default Page
